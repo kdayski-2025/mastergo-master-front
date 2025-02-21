6b6988592f581c8904e44efaf8becf2e8645379b
kdayski <58617773+kdayski@users.noreply.github.com>
2025-02-21 12:42:39 +0500

    fix

--- pages/RequestDetails/RequestDetails.jsx
+++ pages/RequestDetails/RequestDetails.jsx
@@ -19,11 +19,11 @@ export default function RequestDetailsScreen({ route }) {
   });
 
   const handleChange = (value) => {
-    setFormData({ price: value });
+    setFormData((prev) => ({ ...prev, price: value }));
   };
 
   const handleCommentChange = (value) => {
-    setFormData({ comment: value });
+    setFormData((prev) => ({ ...prev, comment: value }));
   };
 
   useEffect(() => {
